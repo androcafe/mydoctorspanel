43e95b5a5ae3d4ef36e01ff90c3b011a02f9f114
Yayati09 <yaaytivs@gmail.com>
2018-12-15 15:53:41 +0530

    Initial Commit

---
+++ app/src/main/java/androcafe/visitindia/com/mydoctorspanel/DoctorPanelUrl.java
@@ -0,0 +1,21 @@
+package androcafe.visitindia.com.mydoctorspanel;
+
+public interface DoctorPanelUrl {
+
+    String SIGN_UP_URL="https://doctor.codingcloud.com.my/doctor_signup.php";
+
+    String SIGN_IN_URL="https://doctor.codingcloud.com.my/doctor_signin1.php";
+
+    String INSERT_PROFILE_URL="https://doctor.codingcloud.com.my/doctor_info_up.php";
+
+    String CANCEL_APMT_URL="";
+
+    String CHANGE_SCHED_URL="";
+
+    String TODAYS_APMT_URL="";
+
+    String ALL_APMT_URL="https://doctor.codingcloud.com.my/appointment_booking_details.php";
+    
+    String PATIENT_INFO_URL="https://doctor.codingcloud.com.my/patient_info1.php";
+
+}
